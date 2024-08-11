36679135cc8bbae40c205cd411c44ffccb291769
Elena Andreeva <elena.and.kam@gmail.com>
2024-08-12 00:54:26 +1200

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    sortedArr = [];
+    if (!matrix || !matrix.length) {
+        return sortedArr;
+    } else {
+        for (let i = 0; i < matrix.length; i++) {
+            if (i == 0 || i % 2 == 0) {
+                sortedArr.push(matrix[i]);
+            } else {
+                sortedArr.push(matrix[i].reverse());
+            }
+        }
+        sortedArr = sortedArr.join(",").split(",");
+        return (sortedArr = sortedArr.map((elem) => elem * 1));
+    }
+};
